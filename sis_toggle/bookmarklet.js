87883913aaafe5dd07d2a6e5e073dbe8de46beb5
David Varney <davidvarney@gmail.com>
2016-10-17 12:46:18 -0400

    SIS Toggle: Testing

--- sis_toggle/bookmarklet.js
+++ sis_toggle/bookmarklet.js
@@ -1,6 +1,16 @@
 //(function(){
 var current_url = window.location.host;
 var uri = window.location.pathname;
+var params = window.location.search.substring(1);
 console.log("URL: " + current_url);
 console.log("URI: " + uri);
+console.log("Params: " + params);
+switch (current_url) {
+    case 'sis2.focuslearn.org':
+        window.location = window.location.protocol + "//sis2.dev" + uri +
+    break;
+    case 'sis2.dev':
+
+    break;
+}
 //});
