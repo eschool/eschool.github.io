f43133db346ba2d3ea53f5b40cc00a2c6e72821e
David Varney <davidvarney@gmail.com>
2016-10-17 12:51:46 -0400

    Adds the finishing touches to the SIS Toggle bookmarklet.

--- sis_toggle/bookmarklet.js
+++ sis_toggle/bookmarklet.js
@@ -1,16 +1,16 @@
-//(function(){
+var protocol = window.location.protocol;
 var current_url = window.location.host;
 var uri = window.location.pathname;
 var params = window.location.search.substring(1);
+console.log("Protocol: " + protocol);
 console.log("URL: " + current_url);
 console.log("URI: " + uri);
 console.log("Params: " + params);
 switch (current_url) {
     case 'sis2.focuslearn.org':
-        window.location = window.location.protocol + "//sis2.dev" + uri + "";
+        window.location = protocol + "//sis2.dev" + uri + "?" + params;
     break;
     case 'sis2.dev':
-
+        window.location = protocol + "//sis2.focuslearn.org" + uri + "?" + params;
     break;
 }
-//});
